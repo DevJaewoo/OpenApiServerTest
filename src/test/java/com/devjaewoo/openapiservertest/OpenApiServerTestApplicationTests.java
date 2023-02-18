4925148dd261426fba2995b633c8fc36b9972e1c
Jaewoo Ahn <DevJaewoo@gmail.com>
2023-02-18 10:48:35 +0900

    Initial Commit

---
+++ src/test/java/com/devjaewoo/openapiservertest/OpenApiServerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjaewoo.openapiservertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiServerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
