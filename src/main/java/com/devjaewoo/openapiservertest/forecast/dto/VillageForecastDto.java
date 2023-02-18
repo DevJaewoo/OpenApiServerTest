0520a2bd8684b22dccdaf3902fcbf68d19c78f31
Jaewoo Ahn <DevJaewoo@gmail.com>
2023-02-19 08:55:36 +0900

    Chore: DTO 클래스명 및 필드 순서 변경

---
+++ src/main/java/com/devjaewoo/openapiservertest/forecast/dto/VillageForecastDto.java
@@ -0,0 +1,32 @@
+package com.devjaewoo.openapiservertest.forecast.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record VillageForecastDto(
+
+		@JsonProperty("baseDate")
+		String baseDate,
+
+		@JsonProperty("baseTime")
+		String baseTime,
+
+		@JsonProperty("category")
+		String category,
+
+		@JsonProperty("fcstDate")
+		String fcstDate,
+
+		@JsonProperty("fcstTime")
+		String fcstTime,
+
+		@JsonProperty("fcstValue")
+		String fcstValue,
+
+		@JsonProperty("nx")
+		int nx,
+
+		@JsonProperty("ny")
+		int ny
+
+) implements ForecastItem {
+}
