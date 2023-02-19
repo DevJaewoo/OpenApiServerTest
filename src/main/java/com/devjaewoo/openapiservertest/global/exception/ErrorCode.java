7b0885043f4c24f90495cb3b889aff39b0513138
Jaewoo Ahn <DevJaewoo@gmail.com>
2023-02-19 11:39:16 +0900

    Feat: Error Handler 추가

---
+++ src/main/java/com/devjaewoo/openapiservertest/global/exception/ErrorCode.java
@@ -0,0 +1,9 @@
+package com.devjaewoo.openapiservertest.global.exception;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCode {
+    String name();
+    HttpStatus getHttpStatus();
+    String getMessage();
+}
