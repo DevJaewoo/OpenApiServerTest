ec89ae068eff2fa07d560a55d20cc0a6a1cf7869
Jaewoo Ahn <DevJaewoo@gmail.com>
2023-02-21 23:16:23 +0900

    Fix: Forecast 관련 Service, Controller 생성

---
+++ src/main/java/com/devjaewoo/openapiservertest/forecast/controller/ForecastController.java
@@ -0,0 +1,45 @@
+package com.devjaewoo.openapiservertest.forecast.controller;
+
+import com.devjaewoo.openapiservertest.forecast.dto.*;
+import com.devjaewoo.openapiservertest.forecast.service.ForecastService;
+import com.devjaewoo.openapiservertest.forecast.service.OpenApiService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/forecast")
+@RequiredArgsConstructor
+public class ForecastController {
+
+    private final OpenApiService openApiService;
+    private final ForecastService forecastService;
+
+    @GetMapping("/mid")
+    public ResponseEntity<?> getMidForecastData(@Valid MidForecastSearch search) {
+        ForecastResponse<MidForecastDto> response = openApiService.requestMidForecastData(search);
+        response.data().forEach(forecastService::saveMidForecast);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/village")
+    public ResponseEntity<?> getVillageForecastData(@Valid VillageForecastSearch search) {
+        ForecastResponse<VillageForecastDto> response = openApiService.requestVillageForecastData(search);
+        response.data().forEach(forecastService::saveVillageForecast);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/ultra")
+    public ResponseEntity<?> getUltraForecastData(@Valid UltraForecastSearch search) {
+        ForecastResponse<UltraForecastDto> response = openApiService.requestUltraForecastData(search);
+        response.data().forEach(forecastService::saveUltraForecast);
+
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ src/main/java/com/devjaewoo/openapiservertest/forecast/service/ForecastService.java
@@ -0,0 +1,48 @@
+package com.devjaewoo.openapiservertest.forecast.service;
+
+import com.devjaewoo.openapiservertest.forecast.dto.MidForecastDto;
+import com.devjaewoo.openapiservertest.forecast.dto.UltraForecastDto;
+import com.devjaewoo.openapiservertest.forecast.dto.VillageForecastDto;
+import com.devjaewoo.openapiservertest.forecast.entity.MidForecast;
+import com.devjaewoo.openapiservertest.forecast.entity.UltraForecast;
+import com.devjaewoo.openapiservertest.forecast.entity.VillageForecast;
+import com.devjaewoo.openapiservertest.forecast.repository.MidForecastRepository;
+import com.devjaewoo.openapiservertest.forecast.repository.UltraForecastRepository;
+import com.devjaewoo.openapiservertest.forecast.repository.VillageForecastRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ForecastService {
+
+    private final MidForecastRepository midForecastRepository;
+    private final VillageForecastRepository villageForecastRepository;
+    private final UltraForecastRepository ultraForecastRepository;
+
+    @Transactional
+    public Long saveMidForecast(MidForecastDto midForecastDto) {
+        MidForecast midForecast = midForecastDto.toEntity();
+        midForecastRepository.save(midForecast);
+
+        return midForecast.getId();
+    }
+
+    @Transactional
+    public Long saveVillageForecast(VillageForecastDto villageForecastDto) {
+        VillageForecast villageForecast = villageForecastDto.toEntity();
+        villageForecastRepository.save(villageForecast);
+
+        return villageForecast.getId();
+    }
+
+    @Transactional
+    public Long saveUltraForecast(UltraForecastDto ultraForecastDto) {
+        UltraForecast ultraForecast = ultraForecastDto.toEntity();
+        ultraForecastRepository.save(ultraForecast);
+
+        return ultraForecast.getId();
+    }
+}
